d1f88ee564c42cb0f13ac8313126bf29dfc3c8ff
Convery <convery10122@gmail.com>
2015-08-16 16:06:32 +0200

    More NTServer typos.

--- OpenNetPlugin/Source/Network/NTServerManager.h
+++ OpenNetPlugin/Source/Network/NTServerManager.h
@@ -19,6 +19,7 @@ class NTServerManager
 {
     // Internal mappings of servers.
     static std::unordered_map<uint64_t /*ServerIPv6*/, uint32_t /*ServerIPv4*/>     Host_ProxyAddresses;
+    static std::unordered_map<uint16_t /*Old port*/, uint16_t /*New port*/>         Host_ProxyPorts;
     static std::unordered_map<uint64_t /*ServerIPv6*/, class IServer *>             Host_ServerAddresses;
     static std::unordered_map<void */*Socket*/, class IServer *>                    Host_ConnectedSockets;
     static std::unordered_map<void */*Socket*/, bool /*Blocked*/>                   Host_SocketBlockStatus;
@@ -33,10 +34,10 @@ class NTServerManager
     static int32_t __stdcall    NT_Bind(void *Socket, const sockaddr *Address, int32_t AddressLength);
     static int32_t __stdcall    NT_CloseSocket(void *Socket);
     static int32_t __stdcall    NT_Connect(void *Socket, const sockaddr *Address, int32_t AddressLength);
-    static int32_t __stdcall    NT_GetPeerName(void *Socket, sockaddr *Address, int32_t AddressLength);
-    static int32_t __stdcall    NT_GetSockName(void *Socket, sockaddr *Address, int32_t AddressLength);
+    static int32_t __stdcall    NT_GetPeerName(void *Socket, sockaddr *Address, int32_t *AddressLength);
+    static int32_t __stdcall    NT_GetSockName(void *Socket, sockaddr *Address, int32_t *AddressLength);
     static int32_t __stdcall    NT_GetSockOpt(void *Socket, int32_t Level, int32_t OptionName, char *OptionValue, int32_t *OptionLength);
-    static int32_t __stdcall    NT_IOControlSocket(void *Socket, int32_t Command, uint32_t *ArgumentPointer);
+    static int32_t __stdcall    NT_IOControlSocket(void *Socket, int32_t Command, u_long *ArgumentPointer);
     static int32_t __stdcall    NT_Listen(void *Socket, int32_t Backlog);
     static int32_t __stdcall    NT_Receive(void *Socket, char *Buffer, int32_t BufferLength, int32_t Flags);
     static int32_t __stdcall    NT_ReceiveFrom(void *Socket, char *Buffer, int32_t BufferLength, int32_t Flags, sockaddr *Peer, int32_t *PeerLength);
